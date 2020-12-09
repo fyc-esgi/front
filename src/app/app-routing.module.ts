ee570fdbc8f194641005eb4a2848c1ef0ecbef85
Patrick ZHUANG <patrickzhuang@hotmail.fr>
2020-12-09 23:37:57 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BodyComponent} from './component/body/body.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    component: BodyComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
